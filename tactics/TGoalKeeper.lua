4a003d3c38e1e4c25f77d92f1e1d985d461e6ac8
GersonHMG <gersonhernan00@hotmail.com>
2025-07-15 15:22:37 -0300

    goalkeeper fix

--- tactics/TGoalKeeper.lua
+++ tactics/TGoalKeeper.lua
@@ -1,9 +1,4 @@
-local api = require("sysmickit.engine")
-local Mark = require("skills.mark")
-local move = require("skills.SMove")
-local aim = require("skills.SAim")
-local SCapture = require("skills.SCaptureBall")
-local SPivotKick = require("skills.SPivotKick") -- Usamos la skill de kick fuerte
+local Engine = require("sysmickit.engine")
 local Robot = require("sysmickit.robot")
 
 local TGoalkeeper = {}
@@ -14,21 +9,11 @@ function TGoalkeeper.new(id, team)
 
     return setmetatable({
         state = "guard",
-        cooldown = 0,
         robot = keeper,
         team = team
     }, TGoalkeeper)
 end
 
--- Parámetros para tu cancha (ajusta si cambian dimensiones)
-local GOAL_X = { [0] = -4.5, [1] = 4.5 }
-local AREA_X = { [0] = -3.5, [1] = 3.5 }
-local Y_MAX = 0.45
-local R_INTERCEPT = 1
-local COOLDOWN_TICKS = 20  -- espera tras despeje
--- Define aquí a dónde quieres despejar (zona lateral)
-local SAFE_CLEAR = { [0] = {x = -3.5, y = 1.0},
-                     [1] = {x = 3.5, y = 1.0} }
 
 local function compute_intercept_position(team, ball)
     local x_pos = (team == 0) and -4.1 or 4.1  -- inside the box, but not at edge
@@ -50,7 +35,7 @@ local function is_ball_in_goalie_area(team, ball)
 end
 
 function TGoalkeeper:process()
-    local ball = api.get_ball_state()
+    local ball = Engine.get_ball_state()
     local is_ball_inside = is_ball_in_goalie_area(self.team, ball)
     if not is_ball_inside then
         local pos = compute_intercept_position(self.team, ball)
