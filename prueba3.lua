44803de197b6fe6a398f86b1aff1e41d6d64bf9b
Gerson <gersonhernan00@hotmail.com>
2025-07-15 23:00:18 -0300

    defense

--- prueba3.lua
+++
@@ -1,7 +0,0 @@
-local Engine = require("sysmickit.engine")
--- Dentro de tu loop principal:
-
-function process()
-  print(Engine.get_ref_message())
-end
-
--- test_game_controller.lua
+++
@@ -1,8 +0,0 @@
-local simple_pass = require("playbook.SimplePass")
-
-local team = 0
-local play = simple_pass.new(team)
-
-function process()
-    play:process()
-end
